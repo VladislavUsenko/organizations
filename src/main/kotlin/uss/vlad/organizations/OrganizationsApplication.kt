35e9516b2d2f4e510b90342c6613a312001af9f5
Влад Усенко <usenko.vladoslav.i@gmail.com>
2021-10-05 23:55:00 +0600

    create example project

---
+++ src/main/kotlin/uss/vlad/organizations/OrganizationsApplication.kt
@@ -0,0 +1,11 @@
+package uss.vlad.organizations
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class OrganizationsApplication
+
+fun main(args: Array<String>) {
+    runApplication<OrganizationsApplication>(*args)
+}
---
+++ src/test/kotlin/uss/vlad/organizations/OrganizationsApplicationTests.kt
@@ -0,0 +1,13 @@
+package uss.vlad.organizations
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrganizationsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
